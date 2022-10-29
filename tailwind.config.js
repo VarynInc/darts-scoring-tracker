5f5bbd5487c602b52fd76a0366236540a9b8a58a
John Foster <varyndev@gmail.com>
2022-10-29 13:59:49 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ module.exports = {
     theme: {
       extend: {
         backgroundImage: theme => ({
-          'button-open': "url('/assets/empty.png')",
-          'button-closed': "url('/assets/strike-3.png')",
-          'button-strike-1': "url('/assets/strike-1.png')",
-          'button-strike-2': "url('/assets/strike-2.png')"
+          'button-open': "url('./assets/empty.png')",
+          'button-closed': "url('./assets/strike-3.png')",
+          'button-strike-1': "url('./assets/strike-1.png')",
+          'button-strike-2': "url('./assets/strike-2.png')"
         })
       }
     }
